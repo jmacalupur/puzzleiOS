ac6b8a775415a00d7b1fb9da22af51f569854b1a
jonathan macalupu <macalupu.j@gmail.com>
2018-03-05 17:30:00 -0500

    Initial Commit

---
+++ Platzzel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Platzzel
+//
+//  Created by Jonathan Macalupú on 5/03/18.
+//  Copyright © 2018 Jonathan Macalupú. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
